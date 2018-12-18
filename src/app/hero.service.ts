cbf50d879737c997a63429585f924ae4be5515cf
gurvsxen487 <gauravsaxena.cs@gmail.com>
2018-12-18 13:07:01 +0530

    ASYNCHRONUS CALL

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -8,9 +9,16 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  // SYNCHRONUS CALL
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+
+  // ASYNCHRONUS CALL
+  getHeroes(): Observable<Hero[]> {
+    return of (HEROES);
   }
+
 
   constructor() { }
 }
